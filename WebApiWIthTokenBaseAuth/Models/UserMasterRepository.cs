586bb097eb6fd9f9be76f8aad807121736d910c8
vikastate-5435 <67470492+vikastate-5435@users.noreply.github.com>
2020-08-08 13:05:12 +0530

    Add project files.

---
+++ WebApiWIthTokenBaseAuth/Models/UserMasterRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiWIthTokenBaseAuth.Models
+{
+    public class UserMasterRepository : IDisposable
+    {
+        // SECURITY_DBEntities it is your context class
+        SECURITY_DBEntities context = new SECURITY_DBEntities();
+
+        //This method is used to check and validate the user credentials
+        public UserMaster ValidateUser(string username, string password)
+        {
+            return context.UserMasters.FirstOrDefault(user =>
+            user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+            && user.UserPassword == password);
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
